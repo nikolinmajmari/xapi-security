44fcc9dca4ece167372a25617b7a9a85abf35616
Nikolin Majmari <nikolinmajmari30@gmail.com>
2022-05-08 09:54:46 +0200

    initial commit main

---
+++ core/token_storage.ts
@@ -0,0 +1,9 @@
+import { AuthenticableInterface } from "./authenticable.ts";
+import { UserTokenInterface } from "./token.ts";
+
+export interface TokenStorageInterface<T extends AuthenticableInterface>{
+    loadToken():UserTokenInterface<T>|undefined|Promise<UserTokenInterface<T>|undefined>;
+    saveToken(token:UserTokenInterface<T>):void|Promise<void>;
+    clearToken():Promise<void>;
+}
+
---
+++ core/types.ts
@@ -0,0 +1,2 @@
+import { SecurityContextInterface } from "./context.ts";
+export type JsonObject = {[key:string|number]:JsonObject|string|number};
